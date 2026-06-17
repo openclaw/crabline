ae99214fe932ae3896637337147123cdad73d7f7
Dallin Romney <dallinromney@gmail.com>
2026-06-17 13:50:05 -0700

    export runtime provider API

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
 export { resolveTelegramAdapterConfig } from "./providers/builtin/telegram.js";
 export { resolveWhatsAppAdapterConfig } from "./providers/builtin/whatsapp.js";
+export { createRegistry } from "./providers/registry.js";
+export type { Registry } from "./providers/registry.js";
+export type {
+  BuiltinAdapterId,
+  FixtureDefinition,
+  FixtureMode,
+  ManifestDefinition,
+  ProviderConfig,
+  ProviderPlatform,
+} from "./config/schema.js";
+export type {
+  InboundEnvelope,
+  NormalizedTarget,
+  ProbeResult,
+  ProviderAdapter,
+  ProviderContext,
+  SendContext,
+  SendResult,
+  WaitContext,
+  WatchContext,
+} from "./providers/types.js";
